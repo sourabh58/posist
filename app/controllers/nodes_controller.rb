03ba1d45491ff20aff7cba1297c28ddd929ad20b
SahilThakur7 <sahilrd07@gmail.com>
2018-08-30 19:22:08 +0530

    done

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -5,10 +5,35 @@ class NodesController < ApplicationController
     end
 
     def create
+        @node = Node.new(node_params)
+        @node.record = Record.find(params[:record_id])
+
+        if (@node.referenceNodeId)
+            parent_node = Node.find(@node.referenceNodeId)
+
+            if @node.nodeNumber > parent_node.nodeNumber
+                redirect_to root_path
+            else
+                if @node.save!
+                    redirect_to root_path
+                end
+
+            end
+        else
+
+                if @node.save!
+                    redirect_to root_path
+                end
+
+        end
 
     end
 
     def show
 
     end
+end
+
+def node_params
+    params.require(:node).permit(:data,:nodeNumber,:referenceNodeId)
 end
